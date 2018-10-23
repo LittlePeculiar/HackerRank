8c793a759773e6afe91292fdc2aa90d74800b731
Eunjin Kim <jagpavv@gmail.com>
2018-10-23 17:17:51 +0200

    use dateformatter

--- Time Conversion.playground/Contents.swift
+++ Time Conversion.playground/Contents.swift
@@ -4,26 +4,16 @@
  Note: Midnight is 12:00:00AM on a 12-hour clock, and 00:00:00 on a 24-hour clock. Noon is 12:00:00PM on a 12-hour clock, and 12:00:00 on a 24-hour clock.
  */
 
+import Foundation
+
 func timeConversion(s: String) -> String {
-// +12 / PM or AM 제거
-  var timeString = s
-  let hourInt = Int(s.prefix { ($0 != ":") })
-  var militaryTime = ""
+  let dateFormatter = DateFormatter()
+  dateFormatter.dateFormat = "hh:mm:ssa"
 
-  if let hourInt = hourInt {
-  militaryTime = String(hourInt + 12)
-  }
+  let date = dateFormatter.date(from: s)
+  dateFormatter.dateFormat = "HH:mm:ss"
 
-  let hourStartIdx = timeString.index(timeString.startIndex, offsetBy: 0)
-  let hourEndIdx = timeString.index(timeString.startIndex, offsetBy: 2)
-  timeString.replaceSubrange(hourStartIdx..<hourEndIdx, with: militaryTime)
-
-  for _ in 0...1 {
-    timeString.removeLast()
-  }
-
-  return timeString
+  let date24 = dateFormatter.string(from: date!)
+  return date24
 }
-
-var temp = timeConversion(s: "07:23:30PM")
 
