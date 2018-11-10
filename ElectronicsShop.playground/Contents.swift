9b68b803c961d185ac609dde646ce23b58e4bb7c
Eunjin Kim <jagpavv@gmail.com>
2018-11-10 18:01:17 +0100

    getMoney Spent2

--- ElectronicsShop.playground/Contents.swift
+++ ElectronicsShop.playground/Contents.swift
@@ -2,12 +2,13 @@
  Electronics Shop
  */
 
+// this is right 11.97 out of 15
 func getMoneySpent(keyboards: [Int], drives: [Int], b: Int) -> Int {
   var matchedArr = [Int]()
 
   for k in keyboards {
     for d in drives {
-      var sumOfItem = k + d
+      let sumOfItem = k + d
 
       if sumOfItem < b {
       matchedArr.append(sumOfItem)
@@ -19,3 +20,24 @@ func getMoneySpent(keyboards: [Int], drives: [Int], b: Int) -> Int {
 }
 
 let a = getMoneySpent(keyboards: [3, 1], drives: [5, 2, 8], b: 10)
+
+
+func getMoneySpent2(keyboards: [Int], drives: [Int], b: Int) -> Int {
+  var matched = [Int]()
+  let money = b
+
+  for k in keyboards {
+    let rest = money - k
+    let matchedDrives = drives.filter { $0 <= rest }
+
+    if matchedDrives.count > 0 {
+      matched.append(matchedDrives.max()! + k)
+    }
+  }
+
+  if matched.count > 0 {
+    return matched.max()!
+  } else {
+    return -1
+  }
+}
